79df1d36987841d9f00cc422f04d07fd6cbb5217
Junkun Chen <immortalco233@gmail.com>
2022-07-19 15:06:19 -0500

    remove useless file

--- def.sh
+++
@@ -1,8 +0,0 @@
-function run() {
-	for ((i=0;i<1;++i)); do
-		prog=$1
-		g++ $prog.cpp -o ./$prog -O2 -std=c++14 -Wall || (echo "Compilation Error!" >&2 && break);
-		shift
-		./$prog $@ || (echo "Runtime Error $?" >&2);
-	done
-}
